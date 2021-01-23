f78046a29ada7d2abe6e5f03be43d306504a26f1
excitexcite <excitenew@gmail.com>
2021-01-23 06:06:53 +0200

    Add some delay between lizard spawn moment and the moment when he starts moving

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,7 +6,7 @@ public class Attacker : MonoBehaviour
 {
     
     [Range (0f, 5f)] // adds a slider in unity to adjust attacker speed
-    [SerializeField] float walkSpeed = 1f; // the default attacker speed value
+    [SerializeField] float moveSpeed = 1f; // the default attacker speed value
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,9 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime);
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
     }
+
+    public void setMovementSpeed(float speed) { moveSpeed = speed; }
+
 }
