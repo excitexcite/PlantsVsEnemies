0615d41caa0d53a9064f910b5a9a1e71a85b4782
excitexcite <excitenew@gmail.com>
2021-01-25 03:25:13 +0200

    Add ability to control defender animation from script

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,24 +8,30 @@ public class Shooter : MonoBehaviour
 
     [SerializeField] GameObject projectile; // prefab that represens a projectile
     [SerializeField] GameObject gun; // empty game object that is placed where the projectile has to be instatiated
-    AttackerSpawner myLaneSpawner; // 
+    AttackerSpawner myLaneSpawner; // the line, which is the same to the attacker`s lane
+    Animator animator; // animator object that allows us to control animation events
 
     // Start is called before the first frame update
     void Start()
     {
         SetLaneSpawner();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if there is an attacker on the line defender stars to attack
         if (IsAttackerInLane())
         {
-            Debug.Log("shoot pew pew");
+            //Debug.Log("shoot pew pew");
+            animator.SetBool("IsAttacking", true); // gettuing bool condition from the animator in unity
         }
+        // otherwise defender starts to do his default animation
         else
         {
-            Debug.Log("sit and wane");
+            //Debug.Log("sit and wane");
+            animator.SetBool("IsAttacking", false);
         }
     }
 
